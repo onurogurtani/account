1064014fcbf729b1b58a75044a5ed1710c8f1080
atakan yurttutan <atakan.yurttutan@kgteknoloji.com>
2022-12-05 14:09:10 +0300

    target sentence add

--- src/store/store.js
+++ src/store/store.js
@@ -35,7 +35,7 @@ import { eventsSlice } from './slice/eventsSlice';
 import { classStageSlice } from './slice/classStageSlice';
 import { announcementTypeSlice } from './slice/announcementTypeSlice';
 import { educationYearsSlice } from './slice/educationYearsSlice';
-
+import { targetSentenceSlice } from './slice/targetSentenceSlice';
 const reducers = combineReducers({
   auth: authSlice.reducer,
   user: userSlice.reducer,
@@ -59,6 +59,7 @@ const reducers = combineReducers({
   classStages: classStageSlice.reducer,
   announcementTypes: announcementTypeSlice.reducer,
   educationYears: educationYearsSlice.reducer,
+  targetSentence: targetSentenceSlice,
 });
 
 const persistConfig = {
