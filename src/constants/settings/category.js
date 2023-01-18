b96cfeb4bb92500c393b04af425eca1b0fab9a3f
ialtuntas <habib.koc@kgteknoloji.com>
2023-01-18 13:17:10 +0300

    added new category code

--- src/constants/settings/category.js
+++ src/constants/settings/category.js
@@ -1,1 +1,4 @@
-export const categoryCodes = [{ code: 'questionSolution', value: 'Soru Çözüm' }];
+export const categoryCodes = [
+  { code: 'lectureVideo', value: 'Konu Anlatım Video' },
+  { code: 'solutionVideo', value: 'Soru Çözümlü Video' },
+];
