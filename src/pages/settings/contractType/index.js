a020c4d5c435ec69e1f0d874153db82f6deb2c08
Yasin okunakol <yasinokunakol@gmail.com>
2023-02-03 10:56:38 +0300

    confirm dialog bugfix fixed

--- src/pages/settings/contractType/index.js
+++ src/pages/settings/contractType/index.js
@@ -163,10 +163,12 @@ const ContractTypes = () => {
     await form.validateFields();
     confirmDialog({
       title: 'Uyarı',
-      message: 'Seçtiğiniz Kayıt Üzerinde Değişiklik Yapılacaktır. Emin misiniz?',
+      message: 'Güncellemekte olduğunuz kayıt Sözleşme Türü, Paket Yönetimi, Sözleşmeler ekranında tanımlı olan kayıtları etkileyeceği için Güncelleme yapmak istediğinizden Emin misiniz ?',
       onOk: () => {
         form.submit();
       },
+      okText: 'Evet',
+      cancelText: 'Hayır',
     });
   };
 
@@ -239,7 +241,6 @@ const ContractTypes = () => {
             <CustomInput placeholder="Sözleşme Tipi Adı" />
           </CustomFormItem>
           <CustomFormItem
-            rules={[{ required: true, message: 'Lütfen Zorunlu Alanları Doldurunuz.' }]}
             name={'description'}
             label={<Text t="Açıklama" />}
           >
