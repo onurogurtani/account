f253a46580b6e8958bb284f50c4006e133287d9e
atakan yurttutan <atakan.yurttutan@kgteknoloji.com>
2023-02-18 01:00:00 +0300

    news

--- src/services/maxNetnumber.services.js
+++ src/services/maxNetnumber.services.js
@@ -1,30 +1,30 @@
 import { api } from './api';
 
 const getMaxNetCounts = (data = null) => {
-  return api({
-    url: 'Target/MaxNetCounts/GetByFilterPagedMaxNetCounts',
-    method: 'POST',
-    data,
-  });
+    return api({
+        url: 'Target/MaxNetCounts/GetByFilterPagedMaxNetCounts',
+        method: 'POST',
+        data,
+    });
 };
 const getMaxNetCountsAdd = (data = null) => {
-  return api({
-    url: 'Target/MaxNetCounts/Add',
-    method: 'POST',
-    data,
-  });
+    return api({
+        url: 'Target/MaxNetCounts/Add',
+        method: 'POST',
+        data,
+    });
 };
 const getMaxNetCountsUpdate = (data = null) => {
-  return api({
-    url: 'Target/MaxNetCounts/Update',
-    method: 'POST',
-    data,
-  });
+    return api({
+        url: 'Target/MaxNetCounts/Update',
+        method: 'PUT',
+        data,
+    });
 };
 const maxNetNumberServices = {
-  getMaxNetCounts,
-  getMaxNetCountsAdd,
-  getMaxNetCountsUpdate,
+    getMaxNetCounts,
+    getMaxNetCountsAdd,
+    getMaxNetCountsUpdate,
 };
 
 export default maxNetNumberServices;
