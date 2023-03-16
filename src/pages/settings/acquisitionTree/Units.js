abd2233cfbcf3bcb4874c4da6d23dfb3a3bfb2f3
ialtuntas <habib.koc@kgteknoloji.com>
2023-03-16 10:45:12 +0300

    Added title visibility control

--- src/pages/settings/acquisitionTree/Units.js
+++ src/pages/settings/acquisitionTree/Units.js
@@ -11,63 +11,60 @@ import Unit from './Unit';
 const { Panel } = Collapse;
 const { Title } = Typography;
 const Units = ({ lesson, selectedInsertKey, setSelectedInsertKey }) => {
-  const dispatch = useDispatch();
-  const { lessonUnits } = useSelector((state) => state?.lessonUnits);
-  const [openedPanels, setOpenedPanels] = useState([]);
+    const dispatch = useDispatch();
+    const { lessonUnits } = useSelector((state) => state?.lessonUnits);
+    const [openedPanels, setOpenedPanels] = useState([]);
 
-  const filteredLessonUnits = useMemo(
-    () => lessonUnits?.filter((item) => item.lessonId === lesson.id),
-    [lessonUnits, lesson.id],
-  );
+    const filteredLessonUnits = useMemo(
+        () => lessonUnits?.filter((item) => item.lessonId === lesson.id),
+        [lessonUnits, lesson.id],
+    );
 
-  const onChange = (key) => {
-    setOpenedPanels(key);
-    if (!key.toString()) return false;
-    dispatch(getLessonSubjects(getListFilterParams('lessonUnitId', Number(key.at(-1).toString()))));
-  };
-  const addUnit = async (value) => {
-    const entity = {
-      entity: {
-        name: value,
-        isActive: true,
-        lessonId: lesson.id,
-      },
+    const onChange = (key) => {
+        setOpenedPanels(key);
+        if (!key.toString()) return false;
+        dispatch(getLessonSubjects(getListFilterParams('lessonUnitId', Number(key.at(-1).toString()))));
     };
-    await dispatch(addUnits(entity));
-  };
-  return (
-    <>
-      {filteredLessonUnits.length > 0 && <Title level={3}>Üniteler</Title>}
-      <EditableInput
-        height="40"
-        isEdit={lesson.id === selectedInsertKey?.id && selectedInsertKey?.type === 'lesson'}
-        setIsEdit={setSelectedInsertKey}
-        onEnter={addUnit}
-      />
-      <Collapse onChange={onChange}>
-        {filteredLessonUnits
-          .sort((a, b) => a.name.localeCompare(b.name))
-          .map((unit) => (
-            <Panel
-              header={
-                <Unit
-                  setSelectedInsertKey={setSelectedInsertKey}
-                  unit={unit}
-                  open={openedPanels.includes(unit.id.toString())}
-                />
-              }
-              key={unit.id}
-            >
-              <LessonSubjects
-                unit={unit}
-                setSelectedInsertKey={setSelectedInsertKey}
-                selectedInsertKey={selectedInsertKey}
-              />
-            </Panel>
-          ))}
-      </Collapse>
-    </>
-  );
+    const addUnit = async (value) => {
+        const entity = {
+            entity: {
+                name: value,
+                isActive: true,
+                lessonId: lesson.id,
+            },
+        };
+        await dispatch(addUnits(entity));
+    };
+    const isEdit = lesson.id === selectedInsertKey?.id && selectedInsertKey?.type === 'lesson';
+    return (
+        <>
+            {(isEdit || filteredLessonUnits.length !== 0) && <Title level={3}>Üniteler</Title>}
+            <br />
+            <EditableInput height="40" isEdit={isEdit} setIsEdit={setSelectedInsertKey} onEnter={addUnit} />
+            <Collapse onChange={onChange}>
+                {filteredLessonUnits
+                    .sort((a, b) => a.name.localeCompare(b.name))
+                    .map((unit) => (
+                        <Panel
+                            header={
+                                <Unit
+                                    setSelectedInsertKey={setSelectedInsertKey}
+                                    unit={unit}
+                                    open={openedPanels.includes(unit.id.toString())}
+                                />
+                            }
+                            key={unit.id}
+                        >
+                            <LessonSubjects
+                                unit={unit}
+                                setSelectedInsertKey={setSelectedInsertKey}
+                                selectedInsertKey={selectedInsertKey}
+                            />
+                        </Panel>
+                    ))}
+            </Collapse>
+        </>
+    );
 };
 
 export default memo(Units);
