1da5ecf848a63330c9cd1f7295dd6f6ce3ff334f
ialtuntas <habib.koc@kgteknoloji.com>
2023-03-16 10:17:53 +0300

    changed lessons input and lesson message

--- src/pages/settings/acquisitionTree/Lessons.js
+++ src/pages/settings/acquisitionTree/Lessons.js
@@ -1,6 +1,8 @@
 import { Collapse, Typography } from 'antd';
-import React, { memo, useMemo, useState } from 'react';
+import React, { memo, useCallback, useMemo, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import EditableInput from '../../../components/EditableInput';
+import { addLessons } from '../../../store/slice/lessonsSlice';
 import { getUnits } from '../../../store/slice/lessonUnitsSlice';
 import { getListFilterParams } from '../../../utils/utils';
 import Lesson from './Lesson';
@@ -8,49 +10,71 @@ import Units from './Units';
 
 const { Title } = Typography;
 const { Panel } = Collapse;
-const Lessons = ({ classroomId }) => {
-  const dispatch = useDispatch();
-  const { lessons } = useSelector((state) => state?.lessons);
-  const [openedPanels, setOpenedPanels] = useState([]);
-  const [selectedInsertKey, setSelectedInsertKey] = useState();
-  const filteredLessons = useMemo(
-    () => lessons?.filter((item) => item.classroomId === classroomId),
-    [lessons, classroomId],
-  );
+const Lessons = ({ classroomId, isAdd, setIsAdd }) => {
+    const dispatch = useDispatch();
+    const { lessons } = useSelector((state) => state?.lessons);
+    const [openedPanels, setOpenedPanels] = useState([]);
+    const [selectedInsertKey, setSelectedInsertKey] = useState();
+    const filteredLessons = useMemo(
+        () => lessons?.filter((item) => item.classroomId === classroomId),
+        [lessons, classroomId],
+    );
 
-  const onChange = (key) => {
-    setOpenedPanels(key);
-    if (!key.toString()) return false;
-    dispatch(getUnits(getListFilterParams('lessonId', Number(key.at(-1).toString()))));
-  };
+    const onChange = (key) => {
+        setOpenedPanels(key);
+        if (!key.toString()) return false;
+        dispatch(getUnits(getListFilterParams('lessonId', Number(key.at(-1).toString()))));
+    };
 
-  return (
-    <>
-      {filteredLessons.length > 0 && <Title level={3}>Dersler</Title>}
-      <Collapse onChange={onChange}>
-        {filteredLessons
-          .sort((a, b) => a.name.localeCompare(b.name))
-          .map((lesson) => (
-            <Panel
-              header={
-                <Lesson
-                  setSelectedInsertKey={setSelectedInsertKey}
-                  lesson={lesson}
-                  open={openedPanels.includes(lesson.id.toString())}
-                />
-              }
-              key={lesson.id}
-            >
-              <Units
-                lesson={lesson}
-                setSelectedInsertKey={setSelectedInsertKey}
-                selectedInsertKey={selectedInsertKey}
-              />
-            </Panel>
-          ))}
-      </Collapse>
-    </>
-  );
+    const addLesson = useCallback(
+        async (value) => {
+            const entity = {
+                entity: {
+                    name: value,
+                    isActive: true,
+                    classroomId,
+                },
+            };
+            await dispatch(addLessons(entity));
+        },
+        [classroomId, dispatch],
+    );
+
+    return (
+        <>
+            {filteredLessons.length === 0 && (
+                <Title level={5} type="danger" style={{ textAlign: 'center' }}>
+                    Bu sınıf seviyesine tanımlı bir kazanım ağacı bulunmamaktadır. Eklemek için ders ekle veya excel ile
+                    ekle seçeneğini kullanın.
+                </Title>
+            )}
+            <br />
+            {(isAdd || filteredLessons.length !== 0) && <Title level={3}>Dersler</Title>}
+            <EditableInput height="40" isEdit={isAdd} setIsEdit={setIsAdd} onEnter={addLesson} />
+            <Collapse onChange={onChange}>
+                {filteredLessons
+                    .sort((a, b) => a.name.localeCompare(b.name))
+                    .map((lesson) => (
+                        <Panel
+                            header={
+                                <Lesson
+                                    setSelectedInsertKey={setSelectedInsertKey}
+                                    lesson={lesson}
+                                    open={openedPanels.includes(lesson.id.toString())}
+                                />
+                            }
+                            key={lesson.id}
+                        >
+                            <Units
+                                lesson={lesson}
+                                setSelectedInsertKey={setSelectedInsertKey}
+                                selectedInsertKey={selectedInsertKey}
+                            />
+                        </Panel>
+                    ))}
+            </Collapse>
+        </>
+    );
 };
 
 export default memo(Lessons);
