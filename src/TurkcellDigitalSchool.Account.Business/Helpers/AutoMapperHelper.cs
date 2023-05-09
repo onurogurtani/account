b9943b0ad6aadf986fba935f517d57027cf45efe
orkun.kozan <orkun.kozan@kgteknoloji.com>
2023-05-09 10:39:42 +0300

    AutoMapper Added

---
+++ src/TurkcellDigitalSchool.Account.Business/Helpers/AutoMapperHelper.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using TurkcellDigitalSchool.Account.Domain.Concrete;
+using TurkcellDigitalSchool.Account.Domain.Dtos;
+using TurkcellDigitalSchool.Core.Entities.Dtos;
+
+namespace TurkcellDigitalSchool.Account.Business.Helpers
+{
+    public class AutoMapperHelper : Profile
+    {
+        public AutoMapperHelper()
+        {
+            CreateMap<User, UserDto>().ReverseMap();
+            CreateMap<User, CurrentUserDto>().ReverseMap();
+            CreateMap<School, SchoolExcelDto>().ReverseMap();
+        }
+    }
+}
