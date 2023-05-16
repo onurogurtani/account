aa0a8fc1e30ead11da9f3999e69b4c3e087b36ea
sener <sener.gozmen@kgteknoloji.com>
2023-05-16 19:50:06 +0300

    refactoring

--- src/TurkcellDigitalSchool.Account.Business/Services/User/IUserService.cs
+++ src/TurkcellDigitalSchool.Account.Business/Services/User/IUserService.cs
@@ -1,5 +1,5 @@
-﻿using System.Threading.Tasks;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using TurkcellDigitalSchool.Account.Domain.Dtos;
 
 namespace TurkcellDigitalSchool.Account.Business.Services.User
