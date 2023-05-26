69968c3c708c8381bcf4335b8f97ad302741cb86
sener <sener.gozmen@kgteknoloji.com>
2023-05-26 16:20:04 +0300

    UserProfilingState

---
+++ src/TurkcellDigitalSchool.Account.Business/Handlers/Users/Commands/SetUserProfilingStateCommand.cs
@@ -0,0 +1,54 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using TurkcellDigitalSchool.Account.DataAccess.Abstract;
+using TurkcellDigitalSchool.Common.BusinessAspects;
+using TurkcellDigitalSchool.Common.Constants;
+using TurkcellDigitalSchool.Common.Helpers;
+using TurkcellDigitalSchool.Core.CustomAttribute;
+using TurkcellDigitalSchool.Core.Enums;
+using TurkcellDigitalSchool.Core.Utilities.Results;
+
+namespace TurkcellDigitalSchool.Account.Business.Handlers.Users.Commands
+{
+    /// <summary>
+    /// SetStatus User
+    /// </summary>
+    public class SetUserProfilingStateCommand : IRequest<IResult>
+    {
+        public long Id { get; set; }
+        public int? ProfilingState { get; set; }
+
+        [MessageClassAttr("Kullanıcı Profillememe Durum Set Etme")]
+        public class SetUserProfilingStateCommandHandler : IRequestHandler<SetUserProfilingStateCommand, IResult>
+        {
+            private readonly IUserRepository _userRepository;
+
+            public SetUserProfilingStateCommandHandler(IUserRepository userRepository)
+            {
+                _userRepository = userRepository;
+            }
+
+            [MessageConstAttr(MessageCodeType.Error)]
+            private static string RecordDoesNotExist = Messages.RecordDoesNotExist;
+            [MessageConstAttr(MessageCodeType.Information)]
+            private static string SuccessfulOperation = Messages.SuccessfulOperation;
+
+            [SecuredOperation]
+
+            public async Task<IResult> Handle(SetUserProfilingStateCommand request, CancellationToken cancellationToken)
+            {
+                var record = await _userRepository.GetAsync(x => x.Id == request.Id);
+                if (record == null)
+                    return new ErrorResult(RecordDoesNotExist.PrepareRedisMessage());
+
+                record.ProfilingState = request.ProfilingState;
+
+                _userRepository.Update(record);
+                await _userRepository.SaveChangesAsync();
+                return new SuccessResult(SuccessfulOperation.PrepareRedisMessage());
+            }
+        }
+    }
+}
+
---
+++ src/TurkcellDigitalSchool.Account.DataAccess/DataAccess/Migrations/Postgre/20230526120854_UserProfilingState.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TurkcellDigitalSchool.Account.DataAccess.DataAccess.Migrations.Postgre
+{
+    public partial class UserProfilingState : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "profilingstate",
+                table: "users",
+                type: "integer",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "profilingstate",
+                table: "users");
+        }
+    }
+}
