8528111341ad6074296e4d7a7ab12d98c256528f
orkun.kozan <orkun.kozan@kgteknoloji.com>
2023-06-06 15:31:46 +0300

    consul register operation

---
+++ src/TurkcellDigitalSchool.Account.Api/Controllers/HealthController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TurkcellDigitalSchool.Account.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet("ping")]
+        [AllowAnonymous]
+        public IActionResult Ping()
+        {
+            return Ok("OK");
+        }
+    }
+}
