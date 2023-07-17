8b7a486025e254456a5423924dad169059da1aa5
Şener Gözmen <sener.gozmen@kgteknoloji.com>
2023-07-17 22:07:27 +0300

    EYDT Account

---
+++ src/TurkcellDigitalSchool.Account.Domain/Concrete/ReadOnly/EYDataTransferMap.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using TurkcellDigitalSchool.Core.Entities;
+
+namespace TurkcellDigitalSchool.Account.Domain.Concrete.ReadOnly
+{
+    public class EYDataTransferMap : IReadOnlyEntity
+    {
+        public long Id { get; set; }
+        public bool IsDeleted { get; set; }
+        public string Table { get; set; }
+        public long NewEducationYearId { get; set; }
+        public long OldId { get; set; }
+        public long NewId { get; set; }
+    }
+}
