01df19111bd83b5d25215c9caf927e3298410c68
mustafa sahin <mustafa.sahin@kgteknoloji.com>
2023-08-07 16:58:15 +0300

    deneme

--- src/TurkcellDigitalSchool.Account.Business/Handlers/Roles/ValidationRules/RoleValidators.cs
+++ src/TurkcellDigitalSchool.Account.Business/Handlers/Roles/ValidationRules/RoleValidators.cs
@@ -1,4 +1,4 @@
-using FluentValidation;
+ï»¿using FluentValidation;
 using TurkcellDigitalSchool.Account.Business.Handlers.Roles.Commands;
 using TurkcellDigitalSchool.Core.Common.Constants;
 using TurkcellDigitalSchool.Core.Common.Helpers;
@@ -7,58 +7,58 @@ using TurkcellDigitalSchool.Core.Enums;
 
 namespace TurkcellDigitalSchool.Account.Business.Handlers.Roles.ValidationRules
 {
-    [MessageClassAttr("Rol Ekleme Doðrulayýcýsý")]
+    [MessageClassAttr("Rol Ekleme DoÄŸrulayÄ±cÄ±sÄ±")]
     public class CreateRoleValidator : AbstractValidator<CreateRoleCommand>
     {
-        [MessageConstAttr(MessageCodeType.Error, "Ýsim,Kullanýcý Tipi,Rol Yetki")]
+        [MessageConstAttr(MessageCodeType.Error, "Ä°sim,KullanÄ±cÄ± Tipi,Rol Yetki")]
         private static string FieldIsNotNullOrEmpty = Messages.FieldIsNotNullOrEmpty;
         [MessageConstAttr(MessageCodeType.Error, "2,100")]
         private static string NumberMustBeCharacterLength = Messages.NumberMustBeCharacterLength;
-        [MessageConstAttr(MessageCodeType.Error, "Ýsim")]
+        [MessageConstAttr(MessageCodeType.Error, "Ä°sim")]
         private static string MustBeOnlyletter = Constants.Messages.MustBeOnlyLetter;
         [MessageConstAttr(MessageCodeType.Error)]
         private static string IsInEnumValue = Messages.IsInEnumValue;
         public CreateRoleValidator()
         {
-            RuleFor(x => x.Role.Name).NotEmpty().WithMessage(FieldIsNotNullOrEmpty.PrepareRedisMessage(messageParameters: new object[] { "Ýsim" })).Length(2, 100).WithMessage(NumberMustBeCharacterLength.PrepareRedisMessage(messageParameters: new object[] { "2", "100" })).Matches("^[a-zA-Z0-9ýðüþöçÝÐÜÞÖÇ() ]*$").WithMessage(MustBeOnlyletter.PrepareRedisMessage(messageParameters: new object[] { "Ýsim" }));
-            RuleFor(x => x.Role.UserType).NotEmpty().WithMessage(FieldIsNotNullOrEmpty.PrepareRedisMessage(messageParameters: new object[] { "Kullanýcý Tipi" })).IsInEnum().WithMessage(IsInEnumValue);
+            RuleFor(x => x.Role.Name).NotEmpty().WithMessage(FieldIsNotNullOrEmpty.PrepareRedisMessage(messageParameters: new object[] { "Ä°sim" })).Length(2, 100).WithMessage(NumberMustBeCharacterLength.PrepareRedisMessage(messageParameters: new object[] { "2", "100" })).Matches("^[a-zA-Z0-9Ä±ÄŸÃ¼ÅŸÃ¶Ã§Ä°ÄžÃœÅžÃ–Ã‡() ]*$").WithMessage(MustBeOnlyletter.PrepareRedisMessage(messageParameters: new object[] { "Ä°sim" }));
+            RuleFor(x => x.Role.UserType).NotEmpty().WithMessage(FieldIsNotNullOrEmpty.PrepareRedisMessage(messageParameters: new object[] { "KullanÄ±cÄ± Tipi" })).IsInEnum().WithMessage(IsInEnumValue);
             RuleFor(x => x.Role.RoleClaims).NotEmpty().Must(x => x.Count > 0).WithMessage(FieldIsNotNullOrEmpty.PrepareRedisMessage(messageParameters: new object[] { "Rol Yetki" }));
         }
     }
 
-    [MessageClassAttr("Rol Güncelleme Doðrulayýcýsý")]
+    [MessageClassAttr("Rol GÃ¼ncelleme DoÄŸrulayÄ±cÄ±sÄ±")]
     public class UpdateRoleValidator : AbstractValidator<UpdateRoleCommand>
     {
-        [MessageConstAttr(MessageCodeType.Error, "Id,Ýsim,Kullanýcý Tipi,Rol Yetki")]
+        [MessageConstAttr(MessageCodeType.Error, "Id,Ä°sim,KullanÄ±cÄ± Tipi,Rol Yetki")]
         private static string FieldIsNotNullOrEmpty = Messages.FieldIsNotNullOrEmpty;
         [MessageConstAttr(MessageCodeType.Error, "2,100")]
         private static string NumberMustBeCharacterLength = Messages.NumberMustBeCharacterLength;
-        [MessageConstAttr(MessageCodeType.Error, "Ýsim")]
+        [MessageConstAttr(MessageCodeType.Error, "Ä°sim")]
         private static string MustBeOnlyletter = Constants.Messages.MustBeOnlyLetter;
         [MessageConstAttr(MessageCodeType.Error)]
         private static string IsInEnumValue = Messages.IsInEnumValue;
         public UpdateRoleValidator()
         {
             RuleFor(x => x.Role.Id).NotEmpty().WithMessage(FieldIsNotNullOrEmpty.PrepareRedisMessage(messageParameters: new object[] { "Id" }));
-            RuleFor(x => x.Role.Name).NotEmpty().WithMessage(FieldIsNotNullOrEmpty.PrepareRedisMessage(messageParameters: new object[] { "Ýsim" })).Length(2, 100).WithMessage(NumberMustBeCharacterLength.PrepareRedisMessage(messageParameters: new object[] { "2", "100" })).Matches("^[a-zA-Z0-9ýðüþöçÝÐÜÞÖÇ() ]*$").WithMessage(MustBeOnlyletter.PrepareRedisMessage(messageParameters: new object[] { "Ýsim" }));
-            RuleFor(x => x.Role.UserType).NotEmpty().WithMessage(FieldIsNotNullOrEmpty.PrepareRedisMessage(messageParameters: new object[] { "Kullanýcý Tipi" })).IsInEnum().WithMessage(IsInEnumValue);
+            RuleFor(x => x.Role.Name).NotEmpty().WithMessage(FieldIsNotNullOrEmpty.PrepareRedisMessage(messageParameters: new object[] { "Ä°sim" })).Length(2, 100).WithMessage(NumberMustBeCharacterLength.PrepareRedisMessage(messageParameters: new object[] { "2", "100" })).Matches("^[a-zA-Z0-9Ä±ÄŸÃ¼ÅŸÃ¶Ã§Ä°ÄžÃœÅžÃ–Ã‡() ]*$").WithMessage(MustBeOnlyletter.PrepareRedisMessage(messageParameters: new object[] { "Ä°sim" }));
+            RuleFor(x => x.Role.UserType).NotEmpty().WithMessage(FieldIsNotNullOrEmpty.PrepareRedisMessage(messageParameters: new object[] { "KullanÄ±cÄ± Tipi" })).IsInEnum().WithMessage(IsInEnumValue);
             RuleFor(x => x.Role.RoleClaims).NotEmpty().Must(x => x.Count > 0).WithMessage(FieldIsNotNullOrEmpty.PrepareRedisMessage(messageParameters: new object[] { "Rol Yetki" }));
         }
     }
 
-    [MessageClassAttr("Rol Kopyalama Doðrulayýcýsý")]
+    [MessageClassAttr("Rol Kopyalama DoÄŸrulayÄ±cÄ±sÄ±")]
     public class RoleCopyValidator : AbstractValidator<RoleCopyCommand>
     {
-        [MessageConstAttr(MessageCodeType.Error, "Id,Ýsim")]
+        [MessageConstAttr(MessageCodeType.Error, "Id,Ä°sim")]
         private static string FieldIsNotNullOrEmpty = Messages.FieldIsNotNullOrEmpty;
         [MessageConstAttr(MessageCodeType.Error, "2,100")]
         private static string NumberMustBeCharacterLength = Messages.NumberMustBeCharacterLength;
-        [MessageConstAttr(MessageCodeType.Error, "Ýsim")]
+        [MessageConstAttr(MessageCodeType.Error, "Ä°sim")]
         private static string MustBeOnlyletter = Constants.Messages.MustBeOnlyLetter;
         public RoleCopyValidator()
         {
             RuleFor(x => x.RoleId).NotEmpty().WithMessage(FieldIsNotNullOrEmpty.PrepareRedisMessage(messageParameters: new object[] { "Id" }));
-            RuleFor(x => x.RoleName).NotEmpty().WithMessage(FieldIsNotNullOrEmpty.PrepareRedisMessage(messageParameters: new object[] { "Ýsim" })).Length(2, 100).WithMessage(NumberMustBeCharacterLength.PrepareRedisMessage(messageParameters: new object[] { "2", "100" })).Matches("^[a-zA-Z0-9ýðüþöçÝÐÜÞÖÇ() ]*$").WithMessage(MustBeOnlyletter.PrepareRedisMessage(messageParameters: new object[] { "Ýsim" }));
+            RuleFor(x => x.RoleName).NotEmpty().WithMessage(FieldIsNotNullOrEmpty.PrepareRedisMessage(messageParameters: new object[] { "Ä°sim" })).Length(2, 100).WithMessage(NumberMustBeCharacterLength.PrepareRedisMessage(messageParameters: new object[] { "2", "100" })).Matches("^[a-zA-Z0-9Ä±ÄŸÃ¼ÅŸÃ¶Ã§Ä°ÄžÃœÅžÃ–Ã‡() ]*$").WithMessage(MustBeOnlyletter.PrepareRedisMessage(messageParameters: new object[] { "Ä°sim" }));
         }
     }
 }
