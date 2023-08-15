71922e4787f4a7273b4d3efe17eb9a425626c0bd
seyma.bilici <seyma.bilici@kgteknoloji.com>
2023-08-15 15:02:33 +0300

    UpdateStudentAccessToChat

--- src/TurkcellDigitalSchool.Account.Business/Handlers/Student/Commands/UpdateStudentAccessToChatCommand.cs
+++ src/TurkcellDigitalSchool.Account.Business/Handlers/Student/Commands/UpdateStudentAccessToChatCommand.cs
@@ -5,6 +5,7 @@ using TurkcellDigitalSchool.Account.DataAccess.Abstract;
 using TurkcellDigitalSchool.Core.Behaviors.Atrribute;
 using TurkcellDigitalSchool.Core.Common.Constants;
 using TurkcellDigitalSchool.Core.Utilities.Results;
+using TurkcellDigitalSchool.Core.Utilities.Security.Jwt;
 
 namespace TurkcellDigitalSchool.Account.Business.Handlers.Student.Commands
 {
@@ -13,28 +14,33 @@ namespace TurkcellDigitalSchool.Account.Business.Handlers.Student.Commands
     /// </summary>
     [LogScope]
     public class UpdateStudentAccessToChatCommand : IRequest<IResult>
-    {
-        public long ParentId { get; set; }
+    { 
         public bool StudentAccessToChat { get; set; }
 
         public class UpdateStudentAccessToChatCommandHandler : IRequestHandler<UpdateStudentAccessToChatCommand, IResult>
         {
             private readonly IStudentParentInformationRepository _studentParentInformationRepository;
+            private readonly ITokenHelper _tokenHelper;
 
-            public UpdateStudentAccessToChatCommandHandler(IStudentParentInformationRepository studentParentInformationRepository)
+            public UpdateStudentAccessToChatCommandHandler(IStudentParentInformationRepository studentParentInformationRepository, ITokenHelper tokenHelper)
             {
                 _studentParentInformationRepository = studentParentInformationRepository;
+                _tokenHelper = tokenHelper;
             }
 
             public async Task<IResult> Handle(UpdateStudentAccessToChatCommand request, CancellationToken cancellationToken)
             {
-                var record = await _studentParentInformationRepository.GetAsync(x => x.ParentId == request.ParentId);
-                if (record == null)
+                var userId = _tokenHelper.GetUserIdByCurrentToken();
+                var records = await _studentParentInformationRepository.GetListAsync(x => x.ParentId == userId);
+                if (records == null)
                     return new ErrorResult(Messages.RecordDoesNotExist);
 
-                record.StudentAccessToChat = request.StudentAccessToChat;
+                foreach (var record in records)
+                {
+                    record.StudentAccessToChat = request.StudentAccessToChat;
+                }
 
-                _studentParentInformationRepository.Update(record);
+                _studentParentInformationRepository.UpdateRange(records);
                 await _studentParentInformationRepository.SaveChangesAsync();
                 return new SuccessResult(Messages.SuccessfulOperation);
             }
