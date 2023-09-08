4596ea37eda1fbf707ccc4048a479219816beb3e
furkan korkusuz <furkan.korkusuz@kgteknoloji.com>
2023-09-08 10:22:41 +0300

    bildirim kanalı kontrolü eklendi.

--- src/TurkcellDigitalSchool.Account.Domain/Dtos/UserAddNotificationDto.cs
+++ src/TurkcellDigitalSchool.Account.Domain/Dtos/UserAddNotificationDto.cs
@@ -1,7 +1,13 @@
-﻿namespace TurkcellDigitalSchool.Account.Domain.Dtos
+﻿using TurkcellDigitalSchool.Core.Enums;
+
+namespace TurkcellDigitalSchool.Account.Domain.Dtos
 {
     public class UserAddNotificationDto
     {
         public long Id { get; set; }
+        public string Name { get; set; }
+        public string MobilePhones { get; set; }
+        public string Email { get; set; }
+        public UserType UserType { get; set; }
     }
 }
